cea7b91f3668696aa5b4df891634d962bb5ea630
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-03-12 14:15:21 -0700

    refactored UserWhiskey

--- anorakapi/models/user_whiskey.py
+++ anorakapi/models/user_whiskey.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class UserWhiskey(models.Model):
 
     title = models.CharField(max_length=50)
-    list_image_url = models.CharField(max_length=255)
+    list_img_url = models.CharField(max_length=255)
     notes = models.CharField(max_length=500)
     rating = models.IntegerField()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
