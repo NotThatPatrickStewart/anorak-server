76f25524d1cc7b1ebd2ef8901d443bb4326cebd5
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-03-18 17:10:59 -0600

    changed name of module, removed 'new'

--- anorakapi/models/whiskey.py
+++ anorakapi/models/whiskey.py
@@ -3,7 +3,6 @@ from django.db import models
 class Whiskey(models.Model):
 
     title = models.CharField(max_length=50)
-    comparable = models.CharField(max_length=50)
     list_img_url = models.CharField(max_length=255)
     tag = models.ForeignKey("Tag", on_delete=models.CASCADE)
     region = models.CharField(max_length=50)
