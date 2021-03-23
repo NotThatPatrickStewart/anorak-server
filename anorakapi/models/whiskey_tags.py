74bbc8b91586b2cb2dd84fd516cd4b25e934ee73
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-03-23 15:18:46 -0600

    added related names to both whiskey and tag properties

--- anorakapi/models/whiskey_tags.py
+++ anorakapi/models/whiskey_tags.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class WhiskeyTag(models.Model):
 
-    whiskey = models.ForeignKey("Whiskey", on_delete=models.CASCADE)
-    tag = models.ForeignKey("Tag", on_delete=models.CASCADE)
+    whiskey = models.ForeignKey("Whiskey", on_delete=models.CASCADE, related_name="relatedwhiskey")
+    tag = models.ForeignKey("Tag", on_delete=models.CASCADE, related_name="relatedtag")
     count = models.IntegerField()
     normalized_count = models.IntegerField()
