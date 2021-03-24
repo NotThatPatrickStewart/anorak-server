105b183b10710fa4f3a2d6a359ef1a10dcd03935
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-03-24 15:31:16 -0600

    removed count from model

--- anorakapi/models/whiskey_tags.py
+++ anorakapi/models/whiskey_tags.py
@@ -4,5 +4,4 @@ class WhiskeyTag(models.Model):
 
     whiskey = models.ForeignKey("Whiskey", on_delete=models.CASCADE, related_name="relatedwhiskey")
     tag = models.ForeignKey("Tag", on_delete=models.CASCADE, related_name="relatedtag")
-    count = models.IntegerField()
     normalized_count = models.IntegerField()
