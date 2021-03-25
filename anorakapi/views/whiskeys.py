926c55908dd22bdf49669c1260eb390c750c6114
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-03-24 22:05:48 -0600

    WIP

--- anorakapi/views/whiskeys.py
+++ anorakapi/views/whiskeys.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
-from anorakapi.models import Whiskey, Tag, WhiskeyTag
+from anorakapi.models import Whiskey, Tag, WhiskeyTag, whiskey
 
 class Whiskeys(ViewSet):
 
@@ -35,29 +35,30 @@ class Whiskeys(ViewSet):
         whiskeys = Whiskey.objects.all()
 
         # Support filtering whiskeys by tag
-        # tag = whiskeys.filter(relatedwhiskey__normalized_count=11)
-        max_count = WhiskeyTag.objects.filter(whiskey_id=166).aggregate(Max('normalized_count'))
+        #find a whiskey by title here, so that the filter ˇ can filter on it instead of a hard-coded number
+       
+        max_count = Whiskey.objects.all()
+        search = self.request.query_params.get('title', None) #search is coming back None, 'title' isn't right?
+        if search is not None:
+            max_count = WhiskeyTag.objects.filter(whiskey_id=search).aggregate(Max('normalized_count')) #finds the highest nomalized count for a particular whiskey
+        print("search")
+        print(search)
         print("max_count")
         print(max_count)
        
-        tag = Tag.objects.get(relatedtag__normalized_count=max_count['normalized_count__max'], relatedtag__whiskey_id=166)
+        tag = Tag.objects.get(relatedtag__normalized_count=max_count['normalized_count__max'], relatedtag__whiskey_id=166) #finds the id of the tag associated with that count
         print('tag')
         print(tag.title)
        
         if tag is not None:
-            result = WhiskeyTag.objects.filter(tag_id=tag.id) #This is bringing back a single instance, but would work if I refactor the fixture to not have a uniq pk for each tag instance
-            #find value normalized_count__max and whiskey_id 
-            whiskeys = list(whiskeys.filter(relatedwhiskey__tag_id__title=tag.title))
+            result = WhiskeyTag.objects.filter(tag_id=tag.id).aggregate(Max('normalized_count')) #finds the highest nomalized count for a particular tag  
+
+            whiskeys = Whiskey.objects.filter(relatedwhiskey__normalized_count=result['normalized_count__max'], relatedwhiskey__tag_id=tag.id) #finds the whiskey object from the above result
+            
         print("result")
         print(result)
         print("whiskeys")
         print(whiskeys)
-
-
-
-
-
-
 
         serializer = WhiskeySerializer(
             whiskeys, many=True, context={'request': request})
@@ -75,12 +76,5 @@ class WhiskeySerializer(serializers.ModelSerializer):
     class Meta:
         model = Whiskey
         fields = ('id', 'title', 'list_img_url', 'region', 'price')
-        depth = 2
-
-# class WhiskeyTagSerializer(serializers.ModelSerializer):
-#     """JSON serializer for whiskey tags"""
-#     class Meta:
-#         model = WhiskeyTag
-#         fields = ('id',)
-#         depth = 2
+        depth = 1
         
